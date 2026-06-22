36632b53b9cf8767c491413b3466538d1d3e7efd
Mohammed Saad <mohammed-saad.mohammad-abdulquddus@capgemini.com>
2026-06-22 16:32:00 +0530

    complete successful project build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HomePage    from './pages/HomePage'
+import TopicPage   from './pages/TopicPage'
+import SectionPage from './pages/SectionPage'
+import SearchPage  from './pages/SearchPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/"                           element={<HomePage />} />
+            <Route path="/topic/:slug"                element={<TopicPage />} />
+            <Route path="/topic/:slug/:sectionSlug"   element={<SectionPage />} />
+            <Route path="/search"                     element={<SearchPage />} />
+            <Route path="*"                           element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
+}
+
